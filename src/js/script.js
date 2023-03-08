8e6093d97fc74f5b7df967193fd45a92763fbc5a
Vyacheslav_Rudenko <rudenkoslava787@gmail.com>
2023-03-08 20:52:15 +0200

    1.1

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     let burger = document.querySelector('.header__top-line-burger');
     let burgerMenu = document.querySelector('.header__burger-menu');
+    let body = document.querySelector('body');
 
     burger.addEventListener('click', function(){
-        burger.classList.toggle('active');
+        body.classList.toggle('active')
         burgerMenu.classList.toggle('active');
+    })
+
+    let teamButton = document.querySelector('.team__img-circle');
+    let teamVideo = document.querySelector('.team__img-video');
+
+    teamButton.addEventListener('click', function(){
+        teamVideo.classList.toggle('active');
     })
     })
